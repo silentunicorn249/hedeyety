dd0409b14f0cbfba1d38472dc89830a44cdda9eb
silentunicorn249 <markbassem@hotmail.com>
2024-12-10 02:35:19 +0200

    added auth screens

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:hedeyety/routes.dart';
+
+import '../components/main_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  static String id = "login_screen";
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late String email;
+  late String password;
+
+  void handleLogin() {
+    Navigator.pushNamed(context, AppRoutes.events);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Flexible(
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.jpg'),
+              ),
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            MainTextInput(
+              onChange: (value) {
+                email = value;
+              },
+              hintText: "Enter your email",
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            MainTextInput(
+              onChange: (value) {
+                password = value;
+              },
+              hintText: "Enter your password",
+              hidden: true,
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            MainButton(
+              clbFn: handleLogin,
+              color: Colors.lightBlueAccent,
+              text: "Log in",
+            ),
+            MainButton(
+              clbFn: () {
+                //Implement registration functionality.
+                Navigator.pop(context);
+              },
+              color: Colors.blueGrey,
+              text: "Go Back",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainTextInput extends StatelessWidget {
+  late ValueChanged<String> onChange;
+  late String hintText;
+  bool hidden = false;
+
+  MainTextInput({
+    required this.onChange,
+    required this.hintText,
+    this.hidden = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: hidden,
+      keyboardType: TextInputType.emailAddress,
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.black),
+      onChanged: onChange,
+      decoration: InputDecoration(
+        hintText: hintText,
+        contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:hedeyety/components/main_button.dart';
+
+import '../routes.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  child: Image.asset('images/logo.jpg'),
+                  height: 80,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "Hedeyety",
+                  style: TextStyle(
+                      fontSize: 45.0,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.black),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            MainButton(
+              text: "Log in",
+              clbFn: () {
+                Navigator.pushNamed(context, AppRoutes.login);
+              },
+              color: Colors.lightBlueAccent,
+            ),
+            MainButton(
+              clbFn: () {
+                Navigator.pushNamed(context, AppRoutes.signup);
+              },
+              text: "Sign up",
+              color: Colors.blueAccent,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
